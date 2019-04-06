c5a789d3688377bb421e094303efe8c3f170748c
Jan Allersma <janallersma@gmail.com>
2019-04-06 15:02:18 +0200

    Improve gallery

--- dist/galerie/galerie.js
+++ dist/galerie/galerie.js
@@ -1,9 +1,21 @@
+function addZip(directory) {
+  let content = document.getElementById('afbeeldingen');
+
+  let zipHref = document.createElement('a');
+  zipHref.className = "link";
+  zipHref.href = directory + "/galerie.zip";
+  zipHref.innerHTML = "Klik hier om alle foto's te downloaden.";
+
+  content.insertAdjacentElement('afterend', zipHref);
+}
+
 function loadGallery() {
-  document.getElementById('content').innerHTML = "";
+  document.getElementById('afbeeldingen').innerHTML = "";
 
   switch(window.location.hash) {
     case "#example":
-        loadImages();
+        loadImages("example", ".jpg", 1);
+        addZip("example");
         break;
     default:
         console.log("could not find " + location.hash);
